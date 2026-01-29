78d73fe6deab87b344e40f68ceab61ce795eacad
VarunRhnklmb <varunbhargav26006@gmail.com>
2026-01-29 12:23:17 +0530

    updated AxiosInstances

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -2,12 +2,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-express-smoky.vercel.app",
+  baseURL: "http://localhost:7000",
 });
 
 // Add token to every request
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); 
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
